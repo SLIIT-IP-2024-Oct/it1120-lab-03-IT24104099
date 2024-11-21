2869e09845f53d8134ffaa46abd2ad5bd75817a4
IT24104099 <it24104099@my.sliit.lk>
2024-11-21 12:37:12 +0530

    Add files via upload

---
+++ Q4/IT24104099Lab3Q4.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class IT24104099Lab3Q4 {
+
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+
+	
+		System.out.print("Enter a five digit number:");
+
+		int number = scanner.nextInt();
+
+		for(int i = 10000; i >= 1; i /= 10) {
+
+			int digit = number / i;
+
+			System.out.print(digit + " ");
+
+			number %= i;
+
+		}
+
+		System.out.println();
+
+	}
+
+}
